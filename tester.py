b53d37befb633da0d8d63e11bfa03190a89bf8f8
noviah <noviahyunji@gmail.com>
2024-11-16 13:51:09 +0900

    Fix tester.py output format

--- tester.py
+++ tester.py
@@ -1,13 +1,11 @@
 from geo.utils import calculate_circle_properties
 
 def main():
-    radius = 10  # 반지름 값을 지정합니다.
+    radius = 10  
     c, area = calculate_circle_properties(radius)
 
-    # 기대 결과와 실제 결과를 출력합니다.
     print(f"c = {c}")
     print(f"area = {area}")
 
 if __name__ == "__main__":
     main()
-
